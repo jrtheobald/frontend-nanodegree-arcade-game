8e35e4baf57953a5420feefe71ddd78393418f8f
James R Theobald <jrtheobald_git@icloud.com>
2018-07-07 17:53:14 -0400

    code: Add inbound status and movement handling
    update() was altered to check for the player's position by assigning `false` to inBound so that handleInput() would reset the player's location and prevent movement beyond the boundaries.

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var Enemy = function(speed, x, y) {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
-    this.y = y; //50 150 230
+    this.y = y; // 50 150 230
     this.speed = speed;
 };
 
@@ -17,24 +17,14 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    // let time = Math.floor((Math.random() * 50) + 1 );
 
-    //let time = 500;
-
+    // this ensures the bugs continuously travel across the screen
     if (this.x < 505) {
-        this.dx = this.speed * dt; // time = time * dt * 25;
+        this.dx = this.speed * dt;
         this.x += this.dx;
     } else {
         this.x = 0;
     }
-    
-
-
-
-
-
-    // this.x = 50 * dt;
-    // this.y = 1 * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -51,11 +41,17 @@ var Player = function() {
     this.x = 100;
     this.y = 380;
     this.speed = 0;
+    this.inBound = true;
 };
 
 Player.prototype.update = function(dt) {
     // update player location in response to keypress
     console.log(`${this.x}, ${this.y}`);
+    console.log(`${this.inBound}`);
+    // check to ensure that the player's position is within bounds
+    if (this.x > 410 || this.x < -10 || this.y > 420 || this.y < -40) {
+        this.inBound = false;
+    }
 };
 
 Player.prototype.render = function() {
@@ -63,28 +59,37 @@ Player.prototype.render = function() {
 };
 
 Player.prototype.handleInput = function(direction) {
-    switch (direction) {
-        case 'left':
-            this.x -= 20;
-            break;
-        case 'right':
-            this.x += 20;
-            break;
-        case 'up':
-            this.y -= 20;
-            break;
-        case 'down':
-            this.y += 20;
-            break;
-
+    // if player is within bouns update handle input normally
+    if (this.inBound) {
+        switch (direction) {
+            case 'left':
+                this.x -= 20;
+                break;
+            case 'right':
+                this.x += 20;
+                break;
+            case 'up':
+                this.y -= 20;
+                break;
+            case 'down':
+                this.y += 20;
+                break;
+    
+        }
+    // if the player is out of bounds, reset the player location  
+    } else {
+        this.x = 100;
+        this.y = 300;
+        this.inBound = true;
     }
+    
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-const enemy1 = new Enemy(500, 0, 50);
-const enemy2 = new Enemy(300, 0, 150);
+const enemy1 = new Enemy(100, 0, 50);
+const enemy2 = new Enemy(80, 0, 150);
 const enemy3 = new Enemy(60, 0, 230);
 const allEnemies = [enemy1, enemy2, enemy3];
 const player = new Player();
